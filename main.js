305fff3d6a63d342692d2645c5d4987b2ea6294f
pshenmic <pshenmic@gmail.com>
2019-01-30 20:20:20 +0700

    EditController: add page + fetch controller data

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueApollo from "vue-apollo"
 import Controllers from './components/Controllers.vue'
+import EditController from './components/EditController.vue'
 import Home from './components/Home.vue'
 import App from './components/App.vue'
 import {ApolloClient} from 'apollo-client'
@@ -18,7 +19,6 @@ const httpLink = new HttpLink({
 
 // Cache implementation
 const cache = new InMemoryCache()
-
 
 
 const authMiddleware = new ApolloLink((operation, forward) => {
@@ -45,11 +45,11 @@ Vue.use(VueApollo)
 Vue.use(VueRouter)
 
 
-
 const routes = [
-    { path: '/', redirect: '/home' },
-    { path: '/home', component: Home },
-    { path: '/controllers', component: Controllers }
+    {path: '/', redirect: '/home'},
+    {path: '/home', component: Home},
+    {path: '/controllers', component: Controllers},
+    {path: '/controller/:id/edit', component: EditController}
 ]
 
 
