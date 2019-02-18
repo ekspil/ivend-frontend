45d24b24d2caaa65993cc98ee4b865ffdcfbadb2
Alex <andreewsky.alexey@yandex.ru>
2019-02-18 15:25:59 +0700

    Add automate page and notification settings

--- layout/html/assets/js/alexcustom.js
+++ layout/html/assets/js/alexcustom.js
@@ -4,4 +4,36 @@ window.addEventListener('load', function () {
             disablePastDays: true
         });
     }
-})
+});
+
+jQuery('<div class="number-input__nav"><div class="number-input__button number-input__button--up">+</div><div class="number-input__button number-input__button--down">-</div></div>').insertAfter('.number-input input');
+jQuery('.number-input').each(function() {
+    var spinner = jQuery(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.number-input__button--up'),
+        btnDown = spinner.find('.number-input__button--down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+    btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+            var newVal = oldValue;
+        } else {
+            var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+            var newVal = oldValue;
+        } else {
+            var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+    });
+});
