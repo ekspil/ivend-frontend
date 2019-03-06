a81aea46d42c1a97a89b226f38e4bcb6c266b75e
Samir Amirseidov <famirseidov@gmail.com>
2019-03-06 21:38:34 +0300

    Redirect: Fix of redirecting

--- src/components/billing/Billing.vue
+++ src/components/billing/Billing.vue
@@ -171,10 +171,9 @@
                         const error = head(errors).message || 'Ошибка сервера.';
                         this.$refs.depositHint.show(convertServerError(error));
                     } else {
-                        this.depositStatus = data.status;
-
-                        if (data.redirectUrl) {
-                            window.location.href = data.redirectUrl;
+                        this.depositStatus = data.requestDeposit.status;
+                        if (data.requestDeposit.redirectUrl) {
+                            window.location.href = data.requestDeposit.redirectUrl;
                         }
                     }
                 } catch (error) {
