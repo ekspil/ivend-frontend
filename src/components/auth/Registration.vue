703fe74351592a8d1aefe1a2a35bc58e1b0b3be4
Samir Amirseidov <famirseidov@gmail.com>
2019-03-13 15:40:19 +0300

    Registration: Fixed steps

--- src/components/auth/Registration.vue
+++ src/components/auth/Registration.vue
@@ -1,78 +1,54 @@
 <template>
     <div class="auth-page">
-        <img src="assets/images/brand/logo.png" class="auth-page__logo"/>
-
+        <img src="assets/images/brand/logo.png" class="auth-page__logo" />
         <!-- Регистрация -->
         <div class="auth-block">
             <div class="auth-block__title">Регистрация</div>
-
-            <Validate
-                v-if="schema"
-                formName="register"
-                :schema="schema"
-                ref="register"
-                :card="false"
-                @onSubmit="register"
-                @onSuccess="onSuccess"
-            >
+            <Validate v-if="schema" formName="register" :schema="schema" ref="register" :card="false" @onSubmit="register" @onSuccess="onSuccess">
                 <div class="auth-block__field-container auth-block__field-container--email">
-                    <Field className="auth-block__field" type="email" name="email" formName="register" placeholder="E-Mail"/>
+                    <Field className="auth-block__field" type="email" name="email" formName="register" placeholder="E-Mail" />
                 </div>
-
                 <div class="auth-block__field-container auth-block__field-container--phone">
-                    <Field :masked="true" className="auth-block__field" type="tel" name="phone" placeholder="Телефон"
-                           mask="\+\7 (111) 111 11-11" formName="register"/>
+                    <Field :masked="true" className="auth-block__field" type="tel" name="phone" placeholder="Телефон" mask="\+\7 (111) 111 11-11" formName="register" />
                 </div>
-
-                <!-- Код телефона -->
-                <!-- Скрыто, функционала нет -->
-                <div class="auth-block__field-container auth-block__field-container--phone-code">
-                    <input class="auth-block__field auth-block__field--phone-code" type="text" name="tel-code"
-                           placeholder="Код из смс" required>
+                <div class="auth-block__field-container auth-block__field-container--phone-code" v-if="step > 1">
+                    <input class="auth-block__field auth-block__field--phone-code" type="text" name="tel-code" placeholder="Код из смс" required disabled>
                     <button class="auth-block__phone-code-btn">Выслать код</button>
                     <div class="auth-block__code-message">
-                        Код подтверждения отправлен на номер<br/>
+                        Код подтверждения отправлен на номер<br />
                         +7 (999) 999 9999
                     </div>
                 </div>
-
                 <!-- Второй шаг регистрации -->
-
                 <div class="auth-block__field-container auth-block__field-container--password">
-                    <div class="auth-block__field-container auth-block__field-container--pass">
-                        <Field className="auth-block__field" type="password" name="password" placeholder="Пароль" formName="register"/>
+                    <div class="auth-block__field-container auth-block__field-container--pass" v-if="step > 1">
+                        <Field className="auth-block__field" type="password" name="password" placeholder="Пароль" formName="register" />
                     </div>
-
-                    <div class="auth-block__field-container auth-block__field-container--pass">
-                        <Field className="auth-block__field" type="password" name="rePassword" placeholder="Повторите пароль" formName="register"/>
+                    <div class="auth-block__field-container auth-block__field-container--pass" v-if="step > 1">
+                        <Field className="auth-block__field" type="password" name="rePassword" placeholder="Повторите пароль" formName="register" />
                     </div>
-
                     <button class="auth-block__submit-btn btn btn-primary" type="submit" @click.prevent="$refs.register.submit">Зарегистрироваться</button>
-
-                    <Field type="checkbox" name="agreement" formName="register" className="default-checkbox">
+                    <Field type="checkbox" name="agreement" formName="register" className="default-checkbox" v-if="step > 1">
                         <template slot="label">
                             Я согласен с <a class="auth-block__link" href="#">условиями соглашения</a>
                         </template>
                     </Field>
                 </div>
             </Validate>
-
             <div class="auth-block__link-container">
                 <span class="auth-block__link-title">Уже есть аккаунт?</span>&nbsp;
                 <router-link to="/login" class="auth-block__link">Войти</router-link>
             </div>
         </div>
     </div>
-
 </template>
-
 <style lang="scss">
-    @import "../../../layout/html/assets/scss/main";
-    .validation-error {
-        margin: 0;
-    }
-</style>
+@import "../../../layout/html/assets/scss/main";
 
+.validation-error {
+    margin: 0;
+}
+</style>
 <script>
 import gql from 'graphql-tag';
 
@@ -83,7 +59,9 @@ import Field from '@/modules/validation/Field';
 
 import { equals, omit, bind } from 'ramda';
 import {
-    required, email, check
+    required,
+    email,
+    check
 } from '@/utils/validation';
 
 export default {
@@ -94,37 +72,43 @@ export default {
     },
     data: () => ({
         schema: null,
-        postData: null
+        postData: null,
+
+        step: 1
     }),
     methods: {
-        async register () {
-            const cache = this.$store.getters['cache/data'];
+        async register() {
+            if (this.step === 1) {
+                this.step++;
+            } else {
+                const cache = this.$store.getters['cache/data'];
 
-            try {
-                const { errors, data } = await this.$apollo.mutate({
-                    mutation: gql `
+                try {
+                    const { errors, data } = await this.$apollo.mutate({
+                        mutation: gql `
                             mutation registerUser ($regData: CreateUserInput!) {
                                 registerUser(input: $regData) { email }
                             }
                         `,
-                    variables: {
-                        regData: {
-                            ...(omit(['agreement', 'rePassword'], cache)),
-                            phone: cache.phone.replace(/[()+\s-]/gi, '').slice(1)
+                        variables: {
+                            regData: {
+                                ...(omit(['agreement', 'rePassword'], cache)),
+                                phone: cache.phone.replace(/[()+\s-]/gi, '').slice(1)
+                            }
                         }
-                    }
-                });
+                    });
 
-                this.$refs.register.process({ errors, data, success: 'Переадресация...' });
-            } catch (error) {
-                this.$refs.register.showMessage('error', convertServerError(error.message));
+                    this.$refs.register.process({ errors, data, success: 'Переадресация...' });
+                } catch (error) {
+                    this.$refs.register.showMessage('error', convertServerError(error.message));
+                }
             }
         },
-        async onSuccess ({ token }) {
+        async onSuccess({ token }) {
             const cache = this.$store.getters['cache/data'];
 
             const { data } = await this.$apollo.mutate({
-                mutation: gql`
+                mutation: gql `
                     mutation loginUser ($logData: RequestTokenInput!) {
                         token: requestToken(input: $logData)
                     }
@@ -141,8 +125,8 @@ export default {
             this.$router.push('/home');
         }
     },
-    created () {
-        const rePasswordValidator = bind(function (rePassword) {
+    created() {
+        const rePasswordValidator = bind(function(rePassword) {
             const cache = this.$store.state.cache.register.data;
             return { error: equals(rePassword, cache.password) ? null : 'Пароли не совпадают.' };
         }, this);
