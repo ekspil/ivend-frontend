95c9e7427409eb2612d55189b4178fc33767e371
Samir Amirseidov <famirseidov@gmail.com>
2019-04-21 23:36:47 +0300

    Billing: Added notification when redirecting from Yandex.Kassa

---
+++ src/modules/Alert.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="alert" v-if="visible">
+		{{ message }}
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Alert',
+		props: {
+			message: String
+		},
+		data: () => ({
+			visible: true
+		}),
+		created () {
+			const vm = this;
+			setTimeout(() => {
+				vm.visible = false;
+			}, 5000);
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.alert {
+		border: 1px solid #00dabf;
+		padding: 1.25em;
+		font-size: 1em;
+		width: 100%;
+		background-color: white;
+		color: black;
+	}
+</style>
