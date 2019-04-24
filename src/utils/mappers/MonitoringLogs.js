915edc6291bd433f22eac59c092b3df2b4390491
Samir Amirseidov <famirseidov@gmail.com>
2019-04-25 00:13:58 +0300

    MonitoringLogs: Created mapper for log type

--- src/utils/mappers/MonitoringLogs.js
+++ src/utils/mappers/MonitoringLogs.js
@@ -1,5 +1,16 @@
 import { getTimestamp } from '@/utils';
 
+const mapType = type => {
+	switch (type) {
+		case 'CONNECTION': return 'Связь';
+		case 'COINACCEPTOR': return 'Монетник';
+		case 'BILLACCEPTOR': return 'Купюроприемник';
+		case 'BUS_ERROR': return 'Ошибка шины';
+		case 'REGISTRATION': return 'Регистрация';
+		default: return '?';
+	}
+}
+
 export const getTableHeaders = () => [
     { name: 'Событие', key: 'type' },
     { name: 'Время', key: 'timestamp' },
@@ -7,7 +18,7 @@ export const getTableHeaders = () => [
 ];
 
 export const getTableFields = data => data.map(({ type, timestamp, message }) => ({
-    type,
+    type: mapType(type),
     timestamp: getTimestamp(timestamp),
     message,
     class: ''
