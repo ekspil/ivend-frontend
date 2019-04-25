414c2a4d431b61560df355570738862a88a8ea51
devnulled <devnulled@cryptaz.org>
2019-04-25 21:44:49 +0700

    Revert feature/roles
    This reverts commit 13b5087

--- src/components/Confirm.vue
+++ src/components/Confirm.vue
@@ -1,11 +1,11 @@
 <template>
-	<div class="container">
-		<div class="side-app">
-			<div class="page-header">
-				<h4 class="page-title">Подтвердите аккаунт, чтобы получить доступ к кабинету</h4>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="side-app">
+            <div class="page-header">
+                <h4 class="page-title">Подтвердите аккаунт, чтобы получить доступ к кабинету</h4>
+            </div>
+        </div>
+   	</div>
 </template>
 
 <script>
