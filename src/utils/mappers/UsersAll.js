9728136ae3cf7e083bc010189677b7539556c4ae
ekspil <ekspil@yandex.ru>
2019-09-01 00:42:56 +1000

    Добавил начисление баланса из списка пользователей

--- src/utils/mappers/UsersAll.js
+++ src/utils/mappers/UsersAll.js
@@ -5,6 +5,7 @@ export const getTableHeaders = () => [
     { name: 'ID', key: 'id', unsortable: false , link: true },
     { name: 'Компания', key: 'companyName', unsortable: false , link: true },
     { name: 'ИНН', key: 'inn', unsortable: false  },
+    { name: 'Баланс', key: 'balance', unsortable: false  },
     { name: 'Телефон', key: 'phone', unsortable: false},
     { name: 'Почта', key: 'email', unsortable: false  },
     { name: 'Роль', key: 'role', link: false },
@@ -15,6 +16,7 @@ export const getTableHeaders = () => [
 export const getTableFields = (data, props) => data.map(user => ({
     id: user.id,
     phone: user.phone,
+    balance: user.balance,
     email: user.email,
     role: user.role,
     inn: user.legalInfo ? user.legalInfo.inn : "Не указано",
